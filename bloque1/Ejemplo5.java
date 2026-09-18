60d9f166c353de864de2d28a99e5c6928a30cb0b
Alejandro <aam0063@alu.medac.es>
2026-09-18 14:49:04 +0200

    a

--- bloque1/Ejemplo5.java
+++ bloque1/Ejemplo5.java
@@ -3,36 +3,42 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-//Ejemplo de uso de FileInputStream y FileOutputStream
+// Ejemplo de uso de FileInputStream y FileOutputStream
 
 public class Ejemplo5 {
 
     public static void main(String[] args) {
 
-        String path = "./TEMA01/Ejemplos/texto.txt";
-        String pathEscritura = "./TEMA01/Ejemplos/surya.png";
+        String path = "./tema1/texto.txt";
+        String pathEscritura = "./tema1/surya.png";
 
         try {
             FileInputStream entrada = new FileInputStream(path);
             int data;
+
             while ((data = entrada.read()) != -1) {
                 System.out.print((char) data);
             }
+
             entrada.close();
+
         } catch (Exception e) {
             System.err.println("Error al leer el archivo: " + e.getMessage());
         }
 
         try {
-            String cadena = "Esto es una prueba de escriturá";
+            String cadena = "Esto es una prueba de escritura";
             byte[] arrayBytes = cadena.getBytes();
 
             FileOutputStream output = new FileOutputStream(pathEscritura);
             output.write(arrayBytes);
             output.close();
+
             System.out.println("\nFichero escrito");
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
